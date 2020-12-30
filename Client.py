6a4a137ffc69cc7a3f1b33dfe6c945961a120019
eden-maximof <eden1807m@gmail.com>
2020-12-30 19:16:17 +0200

    client update

--- Client.py
+++ Client.py
@@ -1,15 +1,76 @@
 import socket
+import struct
+import time
+from datetime import datetime
+#import getch
+#import msvcrt
 
-ip = "132.72.66.42"
+team_name = "morAndEden"
+
+magic_cookie = 0xfeedbeef
+offer_message_type = 0x2
+
+#ip = "132.72.66.42"
 port = 2104
-udp_client_socket = socket(AF_INET, SOCK_DGRAM) # the atributtes say that this is UDP : AF_INET - Internet. SOCK_DGRAM - UDP
-
+source_port_udp = 13117 #our udp port! all the serverce try to connect to this port!
 
 running = True
 
 while running:
-    data, address = udp_client_socket.recvfrom(1024) # buffer size is 1024 bytes - WHAT SHOULD IT BE?
-    #print("received message: %s" % data)
 
-    #loop body - send..
+    # we open here a new socket every time (loop)
+    udp_client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP
+    #udp_client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1) # NOT WORKING ! WHAT IS IT?
+    udp_client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)# Enable broadcasting mode
+    udp_client_socket.bind(("", source_port_udp))
+
+    # Looking for a server
+    print("Client started, listening for offer requests...") # inside of outside ?!
+    #we recive the masseg here becuase we want just the first one !
+    data, address = udp_client_socket.recvfrom(2048) # buffer size is 1024 bytes - WHAT SHOULD IT BE?
+    tuple_info =struct.unpack('Ibh', data)  # or 'LBH' ???
+    # check what we recive its ok
+
+    if tuple_info[0] == magic_cookie and tuple_info[1] == offer_message_type: # offer massege
+
+        #Connecting to a server in TCP
+        tcp_dest_port = tuple_info[2] # its the last info we sent in the struct from the server
+        tcp_dest_ip = address[0] # its the same IP address for UDP and TCP
+        print("Received offer from " + tcp_dest_ip + " attempting to connect...")
+        tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tcp_socket.connect((tcp_dest_ip, tcp_dest_port))#create a TCP connection
+        tcp_socket.send((team_name + "\n").encode())
+
+        #Game mode
+        # need to recive wellcame message here!!!
+
+        wellcome_message = tcp_socket.recv(2048).decode()
+        print(wellcome_message)
+
+
+
+        t1 = datetime.now()
+        while (datetime.now() - t1).seconds <= 10:
+            if msvcrt.kbhit():  # <--------
+                char = getch.getch()
+                try:
+                    tcp_socket.send(char.encode())
+                except:
+                    break
+
+            #time.sleep(0.1)
+
+            #char = getch.getch()
+            #char = msvcrt.getch()
+            #char = "a"
+            # try:
+            #     tcp_socket.send(char.encode())
+            # except:
+            #     break
+            time.sleep(0.01) # DELETE!!
+
+        #after 1o sec
+        tcp_socket.close()
+        print("Server disconnected, listening for offer requests...")
+
 
