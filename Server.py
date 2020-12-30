77cc1ed8edcaf4c6359aa4c9c5fd69ead45e525f
eden-maximof <eden1807m@gmail.com>
2020-12-30 19:12:54 +0200

    update1

--- Server.py
+++ Server.py
@@ -1,21 +1,143 @@
 import socket
+import struct
+import threading
 import time
+from datetime import datetime
 
-server_ip = "132.72.66.42"
-server_port = 2104
-udp_server_socket = socket(AF_INET, SOCK_DGRAM) # the atributtes say that this is UDP : AF_INET - Internet. SOCK_DGRAM - UDP
-udp_server_socket.bind((server_ip, server_port))
 
-print(“Server started, listening on IP address ” + server_ip)
+def send_udp_clients():
+    dest_port_udp = 13117  # When we send massege in UDP
+    for j in range(0, 10):
+        offer_message = struct.pack('Ibh', magic_cookie, offer_message_type, server_port_tcp)  # or 'LBH' ???
+        udp_server_socket.sendto(offer_message, ('<broadcast>', dest_port_udp)) # every time we want to send to another client, how to do it?
+        time.sleep(1)# in this time the client need to send answer, accept game or not
 
+
+def press_per_player(tcp_socket, inx, teams_names):
+    num_players = len(teams_names)
+    half_players_num = num_players // 2
+    teams_names1 = teams_names[:num_players // 2]
+    teams_names2 = teams_names[num_players // 2:]
+
+    wellcome_message ="Welcome to Keyboard Spamming Battle Royale.\nGroup 1:\n==\n"
+
+    for name1 in teams_names1:
+        wellcome_message+= name1+"\n"
+    wellcome_message += "Group 2:\n==\n"
+
+    for name2 in teams_names2:
+        wellcome_message+= name2+"\n"
+    wellcome_message += "Start pressing keys on your keyboard as fast as you can!!\n"
+    tcp_socket.send(wellcome_message.encode())
+
+
+    global res
+    tcp_socket.settimeout(10)
+    t1 = datetime.now()
+    while (datetime.now() - t1).seconds <= 10:
+        try:
+            message = tcp_socket.recv(2048).decode() # make sure !
+            res[inx] += len(message) # need to change! we need to do decode first because now its bytes
+        except:
+            break
+    #tcp_socket.close() # just added
+
+
+
+team_name = "morAndEden"
+magic_cookie = 0xfeedbeef
+offer_message_type = 0x2
+#server_ip = "132.72.66.42" # need to check again !!!
+server_port_udp = 2104 # need to check again !!!
+
+#dest_ip_udp = "172.1.0/24" # where do we really use it?
+
+udp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+#udp_server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+udp_server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+udp_server_socket.bind(("", 11345))# need to check again !!!
+
+tcp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcp_server_socket.bind(("", 11467))############
+server_port_tcp = tcp_server_socket.getsockname()[1] #getsockname return (host, port) of source (there is no dest yet)
+
+print("Server started, listening on IP address " + tcp_server_socket.getsockname()[0])# need to check again !!!
+
+global res
 running = True
-dest_port = 13117
-dest_ip = "172.1.0/24"
-
 while running:
-    udp_server_socket.sendto( "“offer”", (dest_ip, dest_port))
+    # Set a timeout so the socket does not block
+    # indefinitely when trying to receive data.
+    udp_server_socket.settimeout(10)
+    tcp_server_socket.settimeout(20)
+    #Waiting for clients
 
-    #loop body
+    teams_names = []
 
-    #timeout 1 sec
+    #Game mode
+    teams_tcp_sockets = []
+
+    #sending offers message
+    thread_send_udp_clients = threading.Thread(target=send_udp_clients)
+    thread_send_udp_clients.start()
+
+    tcp_server_socket.listen()
+    t1 = datetime.now()
+    while (datetime.now() - t1).seconds <= 10:
+
+        try:
+            connection_tcp_socket, address = tcp_server_socket.accept()
+            teams_tcp_sockets.append(connection_tcp_socket)
+            team_name = connection_tcp_socket.recv(2048).decode()
+            teams_names.append(team_name)
+        except socket.timeout as e:
+            break
+
+    num_players = len(teams_names)
+    half_players_num = num_players// 2
+    teams_names1 = teams_names[:num_players// 2]
+    teams_names2 = teams_names[num_players// 2:]
+    #
+    # print("Welcome to Keyboard Spamming Battle Royale.")
+    # print("Group 1:")
+    # print("==")
+    # for name1 in teams_names1:
+    #     print(name1)
+    # print("Group 2:")
+    # print("==")
+    # for name2 in teams_names2:
+    #     print(name2)
+    # print("Start pressing keys on your keyboard as fast as you can!!")
+
+    res = [0] * num_players
+    for i in range(0, num_players):
+        thread_press_per_player = threading.Thread(target=press_per_player, args=(teams_tcp_sockets[i], i, teams_names))
+        thread_press_per_player.start()
+
+    time.sleep(10) # let the players time to press
+    counter_team1 = 0
+    counter_team2 = 0
+
+    for i in range (0,len(teams_names1)):
+        counter_team1 += res[i]
+    for i in range (len(teams_names1),num_players):
+        counter_team2 += res[i]
+    winner = "Group 1"
+    winner_list = teams_names1
+    if counter_team1 < counter_team2:
+        winner = "Group 2"
+        winner_list = teams_names2
+    print("Game over! Group 1 typed in " + str(counter_team1) + " characters. Group 2 typed in " + str(counter_team2) + " characters.")
+    print(winner + " wins!\n")
+    print("Congratulations to the winners:")
+    print("==")
+    for name in winner_list:
+        print(name)
+
+    #closing connections
+    for tcp_socket in teams_tcp_sockets:
+        tcp_socket.close()
+    print("Game over, sending out offer requests...")
+
 
