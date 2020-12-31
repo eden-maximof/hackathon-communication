fd3265cc513c96cb6039884b269d93c0853f7cfb
eden-maximof <eden1807m@gmail.com>
2020-12-31 20:06:01 +0200

    done

--- Client.py
+++ Client.py
@@ -2,54 +2,71 @@ import socket
 import struct
 import time
 from datetime import datetime
-#import msvcrt
-import getch
+import msvcrt
 #from pynput import keyboard
 
 
-def run_client():
-    team_name = "morAndEden" 
-    magic_cookie = 0xfeedbeef
-    offer_message_type = 0x2
-    source_port_udp = 13117 #our udp port! all the serverce try to connect to this port!
 
-    while True:
+class Client:
+    
+    def __init__(self, team_name, magic_cookie, offer_message_type, source_port_udp):
+        self.team_name = team_name
+        self.magic_cookie = magic_cookie
+        self.offer_message_type = offer_message_type
+        self.source_port_udp = source_port_udp
 
-        udp_client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP
-        udp_client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)# Enable broadcasting mode
-        udp_client_socket.bind(("", source_port_udp))
 
-        # Looking for a server
-        print("Client started, listening for offer requests...")
-        #we recive the masseg here becuase we want just the first one !
-        data, address = udp_client_socket.recvfrom(2048)
-        tuple_info =struct.unpack('Ibh', data)
 
-        if tuple_info[0] == magic_cookie and tuple_info[1] == offer_message_type: # offer massege
+    def run_client(self):
 
+        while True:
+            udp_client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP
+            udp_client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)# Enable broadcasting mode
+            udp_client_socket.bind(("", self.source_port_udp))
+
+            # Looking for a server
+            print("Client started, listening for offer requests...")
+            #we recive the masseg here becuase we want just the first one !
+            data, address = udp_client_socket.recvfrom(2048)
+            tuple_info =struct.unpack('Ibh', data)
+            tcp_dest_ip = address[0] # its the same IP address for UDP and TCP
+            tcp_socket = self.check_offer_message(tuple_info, tcp_dest_ip)
+            if tcp_socket != None:
+                self.game_mode(tcp_socket)
+
+    def check_offer_message(self, tuple_info, tcp_dest_ip):
+        """ if its offer message return tcp socket else the function will return none """
+
+        if tuple_info[0] == self.magic_cookie and tuple_info[1] == self.offer_message_type: # offer massege
             #Connecting to a server in TCP
             tcp_dest_port = tuple_info[2] # its the last info we sent in the struct from the server
-            tcp_dest_ip = address[0] # its the same IP address for UDP and TCP
             print("Received offer from " + tcp_dest_ip + " attempting to connect...")
             tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             tcp_socket.connect((tcp_dest_ip, tcp_dest_port))#create a TCP connection
-            tcp_socket.send((team_name + "\n").encode())
+            tcp_socket.send((self.team_name + "\n").encode())
+            return tcp_socket
+        return None
 
-            #Game mode
+    def game_mode(self, tcp_socket):
+        """ handle game mode - for 10 seconde sending every key press to server over TCP """
 
-            wellcome_message = tcp_socket.recv(2048).decode()
-            print(wellcome_message)
-
-            t1 = datetime.now()
-            while (datetime.now() - t1).seconds <= 10:
-                char = getch.getch()              
-                try:
+        wellcome_message = tcp_socket.recv(2048).decode()
+        print(wellcome_message)
+        t1 = datetime.now()
+        while (datetime.now() - t1).seconds <= 10:
+            try:
+                if msvcrt.kbhit():
+                    char = msvcrt.getch()              
                     tcp_socket.send(char) # already encoded
-                except:
-                    break
-                time.sleep(0.1)
-            #after 1o sec
-            tcp_socket.close()
-            print("Server disconnected, listening for offer requests...")
+                else:
+                    time.sleep(0.1)# handle bussy waiting
+            except:
+                break
+        #after 1o sec
+        tcp_socket.close()
+        print("Server disconnected, listening for offer requests...")
 
-run_client()
+
+client = Client(team_name = "EdenAndMor",magic_cookie = 0xfeedbeef,offer_message_type = 0x2, source_port_udp =  13117)
+client.run_client()
+
--- Server.py
+++ Server.py
@@ -5,131 +5,131 @@ import time
 from datetime import datetime
 import scapy.all as scapy
 
+class Server:
 
-def send_udp_clients():
-    dest_port_udp = 13117  # When we send massege in UDP
-    for j in range(0, 10):
-        offer_message = struct.pack('Ibh', magic_cookie, offer_message_type, server_port_tcp)  # or 'LBH' ???
-        udp_server_socket.sendto(offer_message, ('<broadcast>', dest_port_udp)) # every time we want to send to another client, how to do it?
-        time.sleep(1)# in this time the client need to send answer, accept game or not
+    def __init__(self, dest_port_udp, magic_cookie, offer_message_type):
+        self.dest_port_udp = dest_port_udp
+        self.magic_cookie = magic_cookie
+        self.offer_message_type = offer_message_type
+        self.our_ip = scapy.get_if_addr(scapy.conf.iface)
+        self.res = []
+    
+
+    def send_udp_offers(self ,server_port_tcp, udp_server_socket):
+        """ for ten seconds sends offer request every second over UDP with dest_port_udp """
+        for j in range(0, 10):
+            offer_message = struct.pack('Ibh', self.magic_cookie, self.offer_message_type, server_port_tcp)
+            udp_server_socket.sendto(offer_message, ('<broadcast>', self.dest_port_udp)) 
+            time.sleep(1)# in this time the client need to send answer, accept game or not
 
 
-def press_per_player(tcp_socket, inx, teams_names):
-    num_players = len(teams_names)
-    half_players_num = num_players // 2
-    teams_names1 = teams_names[:num_players // 2]
-    teams_names2 = teams_names[num_players // 2:]
-
-    wellcome_message ="Welcome to Keyboard Spamming Battle Royale.\nGroup 1:\n==\n"
-
-    for name1 in teams_names1:
-        wellcome_message+= name1+"\n"
-    wellcome_message += "Group 2:\n==\n"
-
-    for name2 in teams_names2:
-        wellcome_message+= name2+"\n"
-    wellcome_message += "Start pressing keys on your keyboard as fast as you can!!\n"
-    try:
-        tcp_socket.send(wellcome_message.encode())
-    except:
-        pass
-
-    global res
-    tcp_socket.settimeout(10)
-    t1 = datetime.now()
-    while (datetime.now() - t1).seconds <= 10:
+    def press_per_player(self, tcp_socket, inx, wellcome_message):
+        """  listener for every player separatly and update their counter """ 
         try:
-            message = tcp_socket.recv(2048).decode() # make sure !
-            res[inx] += len(message) # need to change! we need to do decode first because now its bytes
+            tcp_socket.send(wellcome_message.encode())
         except:
-            break
-    #tcp_socket.close() # just added
+            pass
+
+        tcp_socket.settimeout(10)
+        t1 = datetime.now()
+        while (datetime.now() - t1).seconds <= 10:
+            try:
+                message = tcp_socket.recv(2048).decode() # make sure !
+                self.res[inx] += len(message) # need to change! we need to do decode first because now its bytes
+            except:
+                break     
+
+    def create_wellcome_message(self, teams_names1, teams_names2):
+        wellcome_message ="Welcome to Keyboard Spamming Battle Royale.\nGroup 1:\n==\n"
+        for name1 in teams_names1:
+            wellcome_message+= name1+"\n"
+        wellcome_message += "Group 2:\n==\n"
+        for name2 in teams_names2:
+            wellcome_message+= name2+"\n"
+        wellcome_message += "Start pressing keys on your keyboard as fast as you can!!\n"
+
+        return wellcome_message
+
+    def listening_tcp_conections(self, tcp_server_socket):
+        """ for ten seconds listen for TCP connection requests, each one is a player"""
+        teams_names = []
+        teams_tcp_sockets = []
+        tcp_server_socket.listen()
+        t1 = datetime.now()
+        while (datetime.now() - t1).seconds <= 10:
+            try:
+                connection_tcp_socket, address = tcp_server_socket.accept()
+                teams_tcp_sockets.append(connection_tcp_socket)
+                team_name = connection_tcp_socket.recv(2048).decode()
+                teams_names.append(team_name)
+            except socket.timeout as e:
+                break
+        return teams_names, teams_tcp_sockets
+
+    def calculate_print_winners(self, teams_names1, teams_names2, num_players):
+        counter_team1 = 0
+        counter_team2 = 0
+        for i in range (0,len(teams_names1)):
+            counter_team1 += self.res[i]
+        for i in range (len(teams_names1),num_players):
+            counter_team2 += self.res[i]
+        winner = "Group 1"
+        winner_list = teams_names1
+        if counter_team1 < counter_team2:
+            winner = "Group 2"
+            winner_list = teams_names2
+        print("Game over! Group 1 typed in " + str(counter_team1) + " characters. Group 2 typed in " + str(counter_team2) + " characters.")
+        print(winner + " wins!\n")
+        print("Congratulations to the winners:")
+        print("==")
+        for name in winner_list:
+            print(name)
 
 
 
-magic_cookie = 0xfeedbeef
-offer_message_type = 0x2
-#server_ip = "132.72.66.42" # need to check again !!!
-server_port_udp = 2104 # need to check again !!!
-our_ip = scapy.get_if_addr(scapy.conf.iface)
+    def run_server(self):
+        #start UDP socket
+        udp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        udp_server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        udp_server_socket.bind((self.our_ip, 11345))
+        #start TCP wellcome socket
+        tcp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tcp_server_socket.bind((self.our_ip, 11467))
+        server_port_tcp = tcp_server_socket.getsockname()[1] #getsockname return (host, port) of source (there is no dest yet)
 
-#dest_ip_udp = "172.1.0/24" # where do we really use it?
+        print("Server started, listening on IP address " + self.our_ip)# need to check again !!!
 
-udp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-#udp_server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-udp_server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        while True:
+            # Set a timeout so the socket does not block
+            # indefinitely when trying to receive data.
+            udp_server_socket.settimeout(10)
+            tcp_server_socket.settimeout(20)
+            
+            #sending offers message - thread
+            thread_send_udp_clients = threading.Thread(target=self.send_udp_offers, args = (server_port_tcp, udp_server_socket,))
+            thread_send_udp_clients.start()
 
-udp_server_socket.bind((our_ip, 11345))# need to check again !!!
+            #meanwhile listening to TCP conections 
+            teams_names, teams_tcp_sockets = self.listening_tcp_conections(tcp_server_socket)
 
-tcp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcp_server_socket.bind((our_ip, 11467))############
-server_port_tcp = tcp_server_socket.getsockname()[1] #getsockname return (host, port) of source (there is no dest yet)
+            #Game mode
+            num_players = len(teams_names)
+            teams_names1 = teams_names[:num_players// 2]
+            teams_names2 = teams_names[num_players// 2:]
+        
+            self.res = [0] * num_players
+            wellcome_message = self.create_wellcome_message(teams_names1, teams_names2)
+            for i in range(0, num_players):
+                thread_press_per_player = threading.Thread(target=self.press_per_player, args=(teams_tcp_sockets[i], i, wellcome_message,))
+                thread_press_per_player.start()
 
-print("Server started, listening on IP address " + tcp_server_socket.getsockname()[0])# need to check again !!!
+            time.sleep(10) # let the players time to press
+            self.calculate_print_winners(teams_names1,teams_names2, num_players)
+            #closing connections
+            for tcp_socket in teams_tcp_sockets:
+                tcp_socket.close()
+            print("Game over, sending out offer requests...")
 
-global res
-running = True
-while running:
-    # Set a timeout so the socket does not block
-    # indefinitely when trying to receive data.
-    udp_server_socket.settimeout(10)
-    tcp_server_socket.settimeout(20)
-    #Waiting for clients
 
-    teams_names = []
-
-    #Game mode
-    teams_tcp_sockets = []
-
-    #sending offers message
-    thread_send_udp_clients = threading.Thread(target=send_udp_clients)
-    thread_send_udp_clients.start()
-
-    tcp_server_socket.listen()
-    t1 = datetime.now()
-    while (datetime.now() - t1).seconds <= 10:
-
-        try:
-            connection_tcp_socket, address = tcp_server_socket.accept()
-            teams_tcp_sockets.append(connection_tcp_socket)
-            team_name = connection_tcp_socket.recv(2048).decode()
-            #print(team_name)
-            teams_names.append(team_name)
-        except socket.timeout as e:
-            break
-
-    num_players = len(teams_names)
-    half_players_num = num_players// 2
-    teams_names1 = teams_names[:num_players// 2]
-    teams_names2 = teams_names[num_players// 2:]
-   
-
-    res = [0] * num_players
-    for i in range(0, num_players):
-        thread_press_per_player = threading.Thread(target=press_per_player, args=(teams_tcp_sockets[i], i, teams_names))
-        thread_press_per_player.start()
-
-    time.sleep(10) # let the players time to press
-    counter_team1 = 0
-    counter_team2 = 0
-
-    for i in range (0,len(teams_names1)):
-        counter_team1 += res[i]
-    for i in range (len(teams_names1),num_players):
-        counter_team2 += res[i]
-    winner = "Group 1"
-    winner_list = teams_names1
-    if counter_team1 < counter_team2:
-        winner = "Group 2"
-        winner_list = teams_names2
-    print("Game over! Group 1 typed in " + str(counter_team1) + " characters. Group 2 typed in " + str(counter_team2) + " characters.")
-    print(winner + " wins!\n")
-    print("Congratulations to the winners:")
-    print("==")
-    for name in winner_list:
-        print(name)
-
-    #closing connections
-    for tcp_socket in teams_tcp_sockets:
-        tcp_socket.close()
-    print("Game over, sending out offer requests...")
+server = Server(dest_port_udp = 13117, magic_cookie = 0xfeedbeef, offer_message_type = 0x2)
+server.run_server()
